dad651c7574c71cf0f428af42cda11e0006be917
Aida Sarbas <aida.sarbas@icloud.com>
2022-02-27 18:44:35 +0600

    ДЗ Неделя13

---
+++ week13/assets/js/index.js
@@ -0,0 +1,32 @@
+function yourName() {
+    let yourName = prompt('Как тебя зовут?');
+    alert(`Привет, ${yourName}!`);
+}
+
+function plus() {
+    let a = prompt('Введите первое значение');
+    let b = prompt('Введите второе значение');
+
+    alert(Number(a) + Number(b));
+}
+
+function minus() {
+    let a = prompt('Введите первое значение');
+    let b = prompt('Введите второе значение');
+
+    alert(Number(a) - Number(b));
+}
+
+function times() {
+    let a = prompt('Введите первое значение');
+    let b = prompt('Введите второе значение');
+
+    alert(Number(a) * Number(b));
+}
+
+function divide() {
+    let a = prompt('Введите первое значение');
+    let b = prompt('Введите второе значение');
+
+    alert(Number(a) / Number(b));
+}
